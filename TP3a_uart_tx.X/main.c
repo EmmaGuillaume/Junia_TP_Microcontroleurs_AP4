1ff07f4cde7fce689cd87ecd7ff832db5e793e65
ctomera <clement.tomera.smb@gmail.com>
2026-02-04 14:51:03 +0100

    tp3a_uart_tx

--- TP3a_uart_tx.X/main.c
+++ TP3a_uart_tx.X/main.c
@@ -1,44 +1,15 @@
 #include "configbits.h" // Bits de configuration
 #include <xc.h>         // Definition des registres specifiques au uC
 
-#define LED1 LATDbits.LATD0
-#define LED2 LATDbits.LATD1
-#define LED3 LATDbits.LATD2
-#define LED4 LATDbits.LATD3
-#define LED5 LATBbits.LATB0
-#define LED6 LATBbits.LATB1
-#define LED7 LATBbits.LATB2
-#define LED8 LATBbits.LATB3
-
-#define DIR_LED1 TRISDbits.TRISD0
-#define DIR_LED2 TRISDbits.TRISD1
-#define DIR_LED3 TRISDbits.TRISD2
-#define DIR_LED4 TRISDbits.TRISD3
-#define DIR_LED5 TRISBbits.TRISB0
-#define DIR_LED6 TRISBbits.TRISB1
-#define DIR_LED7 TRISBbits.TRISB2
-#define DIR_LED8 TRISBbits.TRISB3
-
-void init_leds();
 void delay_approx();
+void config_uart();
+void envoyer_char(char[]);
 const int delay_cycles = 10000; // Nb of delay cycles
 
 void main(void) {
-    init_leds();
-    while (1) {
-
-    }
-}
-
-void init_leds() {
-    DIR_LED1 = 0; LED1 = 0;
-    DIR_LED2 = 0; LED2 = 0;
-    DIR_LED3 = 0; LED3 = 0;
-    DIR_LED4 = 0; LED4 = 0;
-    DIR_LED5 = 0; LED5 = 0;
-    DIR_LED6 = 0; LED6 = 0;
-    DIR_LED7 = 0; LED7 = 0;
-    DIR_LED8 = 0; LED8 = 0;
+    config_uart();
+    char chaine[] = {'H','e','l','l','o',',',' ','w','o','r','l','d',' ','!','\n','\r','\0' };
+    envoyer_char(chaine);
 }
 
 void delay_approx() {
@@ -51,6 +22,7 @@ void config_uart() {
     RC6PPS = 0b10100;
     // Connecter RC7 à RX (via le registre RXPPS)
     RXPPS = 0b10111;
+    
     // RC6 : Sortie
     TRISCbits.TRISC6 = 0;
     // RC7 : Entrée numérique
@@ -65,23 +37,22 @@ void config_uart() {
     BAUD1CONbits.BRG16 = 0;
     // Registres SP1BRGH, SP1BRGL (SP1BRG)
     SP1BRG = 12;
-
-}
-
-void envoyer(char[] ){
+    
     // Activation des pins (Bit SPEN, registre RC1STA)
     RC1STAbits.SPEN = 1;
     // Activation du transmitter (Bit TXEN, registre TX1STA)
     TX1STAbits.TXEN = 1;
-    
-    
     // Activation du receiver (Bit CREN, registre RC1STA)
     RC1STAbits.CREN = 1;
-    // La transmission d?1 octet de données se fait en écrivant dans le registre TX1REG
-    
-   
-    
-    //La réception d?1 octet de données se fait en lisant le registre RC1REG
-
 }
 
+void envoyer_char(char c[]) {
+    int i = 0;
+
+    while (c[i] != '\0') {
+        TX1REG = c[i];
+        i++;
+        delay_approx();
+    }
+  
+}
